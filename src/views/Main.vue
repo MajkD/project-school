a7820e60632496fdc99b1c7505929f739cc1c789
Mikael Danielsson <majkdan@gmail.com>
2021-05-27 14:35:21 +0200

    main wip

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="main">
-    <h1>The Main page!</h1>
+    <div class="selections-panel">
+      <FormInput :title="'Lärare'"></FormInput>
+      <FormInput :title="'Grupp'"></FormInput>
+    </div>
   </div>
 </template>
 
 <script>
+  import FormInput from '../components/FormInput.vue'
+
   export default {
     name: 'Main',
+    components: {
+      FormInput,
+    },
   }
 </script>
+
+<style lang="scss">
+  .main {
+    padding: 1rem;
+  }
+  
+  .selections-panel {
+    width:  20rem;
+  }
+</style>
